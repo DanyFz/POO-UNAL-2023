8be77458349c694649b2b6b980b8011da55d70f7
DanyFz <dalopezza@unal.edu.co>
2023-09-13 23:18:03 -0500

    primero

---
+++ Trabajo 3/Ejercicio_19/src/main/java/com/mycompany/ejercicio_19/Ejercicio_19.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_19;
+
+/**
+ *
+ * @author felip
+ */
+public class Ejercicio_19 {
+
+    public static void main(String[] args) {
+        FORMULARIO form= new FORMULARIO();
+        form.setVisible(true);
+    }
+}

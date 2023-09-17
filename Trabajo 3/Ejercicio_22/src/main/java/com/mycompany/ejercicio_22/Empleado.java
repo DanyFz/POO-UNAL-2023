cbd03e516a5eddf5fa98ecb8b01591eac80e673e
DanyFz <dalopezza@unal.edu.co>
2023-09-17 11:35:18 -0500

    a

---
+++ Trabajo 3/Ejercicio_22/src/main/java/com/mycompany/ejercicio_22/Empleado.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio_22;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Empleado {
+    public String nombre;
+    public double s_hora;
+    public double horas;
+    public double salario;
+    public Empleado(double s_hora, double horas) {
+        this.nombre = nombre;
+        this.horas = horas;
+        this.s_hora = s_hora;
+        this.salario = horas*s_hora;
+    }  
+}    
+  
+    
+
+

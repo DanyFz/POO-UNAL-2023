1e14953cf37d919e4e185ae83981960a17376d14
DanyFz <dalopezza@unal.edu.co>
2023-09-22 12:44:28 -0500

    apuntes

---
+++ Apuntes y Practicas POO/Apuntes/src/main/java/com/mycompany/apuntes/Apuntes.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.apuntes;
+//java distingue netre mayusculas
+//el public o private son los modificadores de acceso
+//las clases y los metodos siempre van con {}, las sentencias terminan en ;
+//enteros= int;entreros pequenos= short; enteros gigantes= long;
+//deciamles de hasta 7 = float, de hasta 15 = double
+//siempre utilizar el mejor mtipo de dato
+//las constantes se declaran con final ejemplo  final souble hoa = 89; asi hoa nunca cambiara su valor
+// <> mayor o menor que, != distinto, == igual que, && es el y logico
+// ++, es operador de incremento, -- es operador de decremento; ambos es de a 1 unidad
+// += n o -= n, es incrementyo o decremento de n unidaddes 
+// api java es para ver la biblioteca e java y sus clases predefinidas 
+
+
+
+
+
+
+//Codigo de ejemplos 
+public class Apuntes{
+    public static void main(String[] args){
+        final double c = 9; // esto es una constatnte, no se le puede cambiarc el valor  
+    }
+}

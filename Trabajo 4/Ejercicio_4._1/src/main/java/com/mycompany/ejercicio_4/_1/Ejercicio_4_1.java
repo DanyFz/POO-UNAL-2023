821b732ecbf76167bff586d6679075bc8865c1ef
DanyFz <dalopezza@unal.edu.co>
2023-10-15 22:21:33 -0500

    4.1

--- Trabajo 4/Ejercicio_4._1/src/main/java/com/mycompany/ejercicio_4/_1/Ejercicio_4_1.java
+++ Trabajo 4/Ejercicio_4._1/src/main/java/com/mycompany/ejercicio_4/_1/Ejercicio_4_1.java
@@ -1,16 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.ejercicio_4._1;
 
-/**
- *
- * @author Usuario
- */
+
+import java.util.*;
 public class Ejercicio_4_1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       Scanner input = new Scanner(System.in);
+       System.out.println("Cuenta de ahorro");
+       System.out.println("Ingrese saldo inicial= $");
+       float saldoInicialAhorros = input.nextFloat(); 
+       System.out.print("Ingrese tasa de interés= ");
+       float tasaAhorros = input.nextFloat();
+       CuentaAhorros cuenta1 = new CuentaAhorros(saldoInicialAhorros, tasaAhorros);
+       System.out.print("Ingresar cantidad a consignar: $");
+       float cantidadDepositar = input.nextFloat();
+       cuenta1.consignar(cantidadDepositar);
+       System.out.print("Ingresar cantidad a retirar: $");
+       float cantidadRetirar = input.nextFloat();
+       cuenta1.retirar(cantidadRetirar);
+       cuenta1.extractoMensual();
+       cuenta1.imprimir();
+
     }
 }

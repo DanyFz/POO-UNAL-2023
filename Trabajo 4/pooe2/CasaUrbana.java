0e08b081e9600907fff9abd9d29576cdde7d8c16
cristian8orduz <141981847+cristian8orduz@users.noreply.github.com>
2023-10-16 11:47:06 -0500

    Add files via upload

---
+++ Trabajo 4/pooe2/CasaUrbana.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pooe2;
+
+/**
+ *
+ * @author Cristian
+ */
+public class CasaUrbana extends Casa {
+    public CasaUrbana(int identificadorInmobiliario, int area, String direccion, int numeroHabitaciones, int numeroBaños, int numeroPisos) {
+        super(identificadorInmobiliario, area, direccion, numeroHabitaciones, numeroBaños, numeroPisos);
+    }
+
+    void imprimir() {
+        super.imprimir();
+    }
+}

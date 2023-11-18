ff5e77397c3fb5a0649798fcf8936f8d32de62b6
jcamilo0321 <144396421+jcamilo0321@users.noreply.github.com>
2023-11-18 15:07:35 -0500

    Add files via upload

---
+++ Trabajo 4/Figuras/src/main/java/com/mycompany/figuras/Figuras.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.figuras;
+
+public class Figuras {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; /* Define la ventana principal */
+        miVentanaPrincipal = new VentanaPrincipal(); /* Crea la ventana principal */
+        miVentanaPrincipal.setVisible(true); /* Establece la ventana como visible */
+        // Establece que la ventana no puede cambiar su tamaño
+        miVentanaPrincipal.setResizable(false);
+    }
+}

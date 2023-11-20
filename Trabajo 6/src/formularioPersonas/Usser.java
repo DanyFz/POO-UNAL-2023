3eef96815b9e42bad18516730cb60f60d8e21186
DanyFz <dalopezza@unal.edu.co>
2023-11-20 17:37:44 -0500

    todo

---
+++ Trabajo 6/src/formularioPersonas/Usser.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package formularioPersonas;
+
+public class Usser {
+    
+      
+    public static void main(String arg[]){
+        
+        
+    }
+      
+    public Usser(){
+        
+        String desktopPath = System.getProperty("user.home") + "\\Desktop";
+        System.out.println(desktopPath);
+        
+    }
+    
+}
